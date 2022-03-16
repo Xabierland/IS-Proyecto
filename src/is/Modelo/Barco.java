99d5e1b8feb512ae3b72c9c19d152e7ab559ebb0
frantxu30git <101074650+frantxu30git@users.noreply.github.com>
2022-03-16 18:34:10 +0100

    confirmar+

---
+++ src/is/Modelo/Barco.java
@@ -0,0 +1,9 @@
+package is.Modelo;
+
+public abstract class Barco {
+    private int longitud;
+    private boolean hundido;
+    private int[] misCoordenadas;
+    public Barco(int pLong,boolean pHund,int[] pCoordenadas){}
+   
+}
