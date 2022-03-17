f95545c3985eb65ab649ac84e50b5552d00211ff
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2022-03-17 02:00:17 +0100

    .

--- src/is/Vista/Menu.java
+++ src/is/Vista/Menu.java
@@ -8,16 +8,31 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.sun.prism.Image;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
+import javax.swing.ImageIcon;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Menu extends JFrame {
 
@@ -30,11 +45,15 @@ public class Menu extends JFrame {
 	private JButton Escudo;
 	private JPanel tablero_jugador;
 	private JPanel tablero_ia;
-	private JPanel panel;
-	private JPanel panel_1;
-	private JLabel lblNewLabel;
-	private JPanel panel_2;
-	private JPanel panel_3;
+	private JPanel barco_panel;
+	private JPanel relleno_abajo;
+	private JPanel relleno_arriba;
+	private Component horizontalStrut;
+	private Component verticalStrut_1;
+	private JLabel lblPlayer;
+	private JLabel lblIa;
+	private Component rigidArea;
+	private JLabel FLECHA;
 
 	/**
 	 * Launch the application.
@@ -60,16 +79,16 @@ public class Menu extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Menu.class.getResource("/resource/icon.png")));
 		setTitle("Hundir la flota");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 751, 468);
+		setBounds(100, 100, 1037, 576);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		contentPane.add(getTableros(), BorderLayout.CENTER);
 		contentPane.add(getArma_panel(), BorderLayout.WEST);
-		contentPane.add(getPanel(), BorderLayout.EAST);
-		contentPane.add(getPanel_2(), BorderLayout.SOUTH);
-		contentPane.add(getPanel_3(), BorderLayout.NORTH);
+		contentPane.add(getBarco_panel(), BorderLayout.EAST);
+		contentPane.add(getRelleno_abajo(), BorderLayout.SOUTH);
+		contentPane.add(getRelleno_arriba(), BorderLayout.NORTH);
 	}
 	private JLabel getCasilla()
 	{
@@ -86,7 +105,7 @@ public class Menu extends JFrame {
 			tableros = new JPanel();
 			tableros.setLayout(new BoxLayout(tableros, BoxLayout.X_AXIS));
 			tableros.add(getTablero_jugador());
-			tableros.add(getLblNewLabel());
+			tableros.add(getHorizontalStrut());
 			tableros.add(getTablero_ia());
 		}
 		return tableros;
@@ -152,36 +171,68 @@ public class Menu extends JFrame {
 		}
 		return tablero_ia;
 	}
-	private JPanel getPanel() {
-		if (panel == null) {
-			panel = new JPanel();
-			panel.setLayout(new GridLayout(10, 1, 0, 0));
-			panel.add(getPanel_1());
+	private JPanel getBarco_panel() {
+		if (barco_panel == null) {
+			barco_panel = new JPanel();
+			barco_panel.setLayout(new GridLayout(0, 1, 0, 0));
+			barco_panel.add(getFLECHA());
 		}
-		return panel;
+		return barco_panel;
 	}
-	private JPanel getPanel_1() {
-		if (panel_1 == null) {
-			panel_1 = new JPanel();
+	private JPanel getRelleno_abajo() {
+		if (relleno_abajo == null) {
+			relleno_abajo = new JPanel();
+			relleno_abajo.setLayout(new GridLayout(0, 1, 0, 0));
+			relleno_abajo.add(getVerticalStrut_1());
 		}
-		return panel_1;
+		return relleno_abajo;
 	}
-	private JLabel getLblNewLabel() {
-		if (lblNewLabel == null) {
-			lblNewLabel = new JLabel("       ");
+	private JPanel getRelleno_arriba() {
+		if (relleno_arriba == null) {
+			relleno_arriba = new JPanel();
+			relleno_arriba.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			relleno_arriba.add(getLblPlayer());
+			relleno_arriba.add(getRigidArea());
+			relleno_arriba.add(getLblIa());
 		}
-		return lblNewLabel;
+		return relleno_arriba;
 	}
-	private JPanel getPanel_2() {
-		if (panel_2 == null) {
-			panel_2 = new JPanel();
+	private Component getHorizontalStrut() {
+		if (horizontalStrut == null) {
+			horizontalStrut = Box.createHorizontalStrut(30);
 		}
-		return panel_2;
+		return horizontalStrut;
 	}
-	private JPanel getPanel_3() {
-		if (panel_3 == null) {
-			panel_3 = new JPanel();
+	private Component getVerticalStrut_1() {
+		if (verticalStrut_1 == null) {
+			verticalStrut_1 = Box.createVerticalStrut(30);
 		}
-		return panel_3;
+		return verticalStrut_1;
+	}
+	private JLabel getLblPlayer() {
+		if (lblPlayer == null) {
+			lblPlayer = new JLabel("Player");
+		}
+		return lblPlayer;
+	}
+	private JLabel getLblIa() {
+		if (lblIa == null) {
+			lblIa = new JLabel("IA");
+		}
+		return lblIa;
+	}
+	private Component getRigidArea() {
+		if (rigidArea == null) {
+			rigidArea = Box.createRigidArea(new Dimension(400, 30));
+		}
+		return rigidArea;
+	}
+	private JLabel getFLECHA() {
+		if (FLECHA == null) {
+			FLECHA = new JLabel("");
+			FLECHA.setVerticalAlignment(SwingConstants.TOP);
+			FLECHA.setIcon(new ImageIcon(Menu.class.getResource("/resource/flecha_ar.png")));
+		}
+		return FLECHA;
 	}
 }
